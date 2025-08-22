e080f9f763c99e78ed356c8f29145103b4d7615e
tarstarcas@gmail.com <ivan.major.888@gmail.com>
2025-08-22 19:49:42 +0300

    adding todo item

---
+++ src/entities/todos/ui/TodoItem.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="todo-item">
+        <p :class="titleClasses">{{ props.todo.title }}</p>
+        <div :class="favoriteClasses">
+            <IconFavorite />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { Todo } from '@/entities/todos/types/todo'
+import IconFavorite from '@/shared/assets/icons/icon-favorite.svg'
+
+const props = defineProps<{
+    todo: Todo
+}>()
+
+const titleClasses = computed(() => [
+    'todo-item__title',
+    { 'todo-item__title---completed': props.todo.completed }
+])
+
+const favoriteClasses = computed(() => [
+    'todo-item__icon',
+   { 'todo-item__icon---favorite': true }
+])
+</script>
+
+<style scoped lang="scss">
+.todo-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+    padding: 12px;
+    border: 1px solid $primary-500;
+    border-radius: 5px;
+
+    @media (min-width: $desktop-breakpoint) {
+        gap: 16px;
+        padding: 16px;
+        border-radius: 10px;
+    }
+
+
+    &__title {
+        font-size: 14px;
+        font-weight: bold;
+        line-height: 20px;
+
+        @media (min-width: $tablet-breakpoint) {
+            font-size: 16px;
+            line-height: 24px;
+        }
+
+        @media (min-width: $desktop-breakpoint) {
+            font-size: 18px;
+            line-height: 28px;
+        }
+
+        @media (min-width: $lg-desktop-breakpoint) {
+            font-size: 24px;
+            line-height: 32px;
+        }
+
+        &---completed {
+            text-decoration: line-through;
+            color: $black-700;
+        }
+    }
+
+    &__icon {
+        flex-shrink: 0;
+        width: 20px;
+        height: 20px;
+        fill: $black-100;
+        stroke: $black-100;
+        cursor: pointer;
+
+        @media (min-width: $tablet-breakpoint) {
+            width: 24px;
+            height: 24px;
+        }
+
+        @media (min-width: $desktop-breakpoint) {
+            width: 28px;
+            height: 28px;
+        }
+
+        @media (min-width: $lg-desktop-breakpoint) {
+            width: 32px;
+            height: 32px;
+        }
+    }
+}
+</style>
